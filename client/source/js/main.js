2ddd01663773a415702a52b39894b198b35d3683
Tom Danvers <tom@tomdanvers.com>
2015-03-30 17:57:36 +0100

    Simple socket.io setup.

--- client/source/js/main.js
+++ client/source/js/main.js
@@ -1,1 +1,16 @@
+
 console.log('Main()');
+
+var io = require('socket.io-client');
+
+var screen = io('http://localhost:8000/screen');
+screen.on('hello', function (data) {
+	console.log('screen', data);
+	screen.emit('my other event', { hello: 'back' });
+});
+
+var input = io('http://localhost:8000/input');
+input.on('hello', function (data) {
+	console.log('input', data);
+	input.emit('my other event', { hello: 'back' });
+});
--- server/source/js/main.js
+++ server/source/js/main.js
@@ -1,8 +1,29 @@
-var http = require('http');
-var server = http.createServer(function (request, response) {
-	console.log(request.url);
-	response.writeHead(200, {'Content-Type': 'text/html' });
-    response.end('<html><body>This is a simple Node Server</body></html>', 'binary');
-});
+var app = require('http').createServer(httpHandler)
+var io = require('socket.io')(app);
 
-server.listen(8000);
+app.listen(8000);
+
+function httpHandler (request, response) {
+
+  	response.writeHead(200, {'Content-Type': 'text/html' });
+    response.end('<html><body>This is a simple Node Server</body></html>', 'binary');
+
+}
+
+var screen = io
+	.of('/screen')
+	.on('connection', function (socket) {
+  		socket.emit('hello', { hello: 'screen' });
+		socket.on('my other event', function (data) {
+    		console.log(data);
+  		});
+	});
+
+var input = io
+	.of('/input')
+	.on('connection', function (socket) {
+  		socket.emit('hello', { hello: 'input' });
+		socket.on('my other event', function (data) {
+    		console.log(data);
+  		});
+	});
